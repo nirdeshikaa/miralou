683af016b1860af37abc4ca18f662980e01b8d29
sagarPakhrin <sagarlama826@gmail.com>
2024-01-05 08:26:02 +0545

    wip: Banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,18 @@
+import HeroImage from '../../assets/images/hero.png';
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className='relative flex items-center'>
+        <img src={HeroImage} />
+        <div className='absolute w-full'>
+          <div className='container mx-auto'>
+            <div className='max-w-48'>
+              <h1 className='text-6xl font-bold'>Scandinavian Supremacy</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
